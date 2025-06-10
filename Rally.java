f7af75db4ac94546d1c8a80c30248b202165ffc6
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-06-10 09:20:11 -0300

    vou arrumar mais tarde

--- Rally.java
+++ Rally.java
@@ -1,9 +1,11 @@
+import java.util.Arrays;
+
 public class Rally {
 
     public static void main (String[] args) {
         int d = 5;
-        int L = 20;
-        int[] n = {7, 9, 12, 14, 18};
+        int L = 21;
+        int[] n = {4, 9, 12, 14, 18, 20};
 
         int[] result = bestRallyPoint(d, L, n);
         for (int i = 0; i < result.length; i++) {
@@ -15,11 +17,30 @@ public class Rally {
         int[] result = new int[pontosParada.length];
         int indexPontosParada = 0;
         int indexResult = 0;
-        int offSet = 0;
+        int offSet = d;
         for (int posicaoAtual = 0; posicaoAtual < L; posicaoAtual++) {
+            offSet--;
+            System.out.println("Ponto de parada: " + pontosParada[indexPontosParada]);
             if(posicaoAtual == pontosParada[indexPontosParada]){
-                if(posicaoAtual + offSet)         
-            }           
+                if(posicaoAtual + offSet == pontosParada[indexPontosParada+1]){
+                    result[indexResult] = pontosParada[indexPontosParada+1];
+                    indexResult++;
+                    indexPontosParada++;
+                    offSet = d;
+                    continue;
+                }else if(posicaoAtual + offSet > pontosParada[indexPontosParada]){
+                    continue;
+                }
+                result[indexResult] = pontosParada[indexPontosParada];
+                offSet = d;
+                indexResult++;
+                indexPontosParada++;
+            } 
+            if(posicaoAtual == pontosParada[pontosParada.length - 1] && posicaoAtual + offSet < L){
+                result[indexResult] = posicaoAtual;
+            }else if(posicaoAtual == pontosParada[pontosParada.length - 1] && posicaoAtual + offSet >= L){
+                result[indexResult] = L;
+            }
         }
         return result;
     }
