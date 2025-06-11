21f7f83bc651990360ceac4b6d771fa05813f60b
Gustavo Hillesheim <g.hillesheim@edu.pucrs.br>
2025-06-11 18:27:03 -0300

    resolvido

--- Rally.java
+++ Rally.java
@@ -1,47 +1,54 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Rally {
 
     public static void main (String[] args) {
         int d = 5;
         int L = 21;
-        int[] n = {4, 9, 12, 14, 18, 20};
+        int[] n = {4, 9, 11,12, 14, 18};
 
-        int[] result = bestRallyPoint(d, L, n);
-        for (int i = 0; i < result.length; i++) {
-            System.out.print(result[i] + " ");
-        }
+
+        var result = bestRallyPoint(d, L, n);
+        System.out.println(result);
     }
 
-    public static int[] bestRallyPoint(int d, int L, int[] pontosParada) {
-        int[] result = new int[pontosParada.length];
-        int indexPontosParada = 0;
-        int indexResult = 0;
-        int offSet = d;
-        for (int posicaoAtual = 0; posicaoAtual < L; posicaoAtual++) {
-            offSet--;
-            System.out.println("Ponto de parada: " + pontosParada[indexPontosParada]);
-            if(posicaoAtual == pontosParada[indexPontosParada]){
-                if(posicaoAtual + offSet == pontosParada[indexPontosParada+1]){
-                    result[indexResult] = pontosParada[indexPontosParada+1];
-                    indexResult++;
-                    indexPontosParada++;
-                    offSet = d;
-                    continue;
-                }else if(posicaoAtual + offSet > pontosParada[indexPontosParada]){
-                    continue;
-                }
-                result[indexResult] = pontosParada[indexPontosParada];
-                offSet = d;
-                indexResult++;
-                indexPontosParada++;
-            } 
-            if(posicaoAtual == pontosParada[pontosParada.length - 1] && posicaoAtual + offSet < L){
-                result[indexResult] = posicaoAtual;
-            }else if(posicaoAtual == pontosParada[pontosParada.length - 1] && posicaoAtual + offSet >= L){
-                result[indexResult] = L;
+    public static List<Integer> bestRallyPoint(int d, int L, int[] pontosParada) {
+        List<Integer> result = new ArrayList<>();
+       
+
+        int offset = d;
+        int pontoAtual = 0;
+        for (int i = 0; i < pontosParada.length-1; i++) {
+            int aux = consegueChegarAte(pontoAtual, offset, pontosParada, i, L);
+            if(aux != -1){
+                result.add(aux);
+                pontoAtual = aux;
+                offset = d;
+                continue;
+            }
+            offset--;
+        }
+        result.add(L);
+        return result;
+    }
+    public static int consegueChegarAte(int posicaoAtual, int offset, int [] pontosDeParada, int start, int L){
+        int resp = -1;
+        
+        for(int idx = start; idx < pontosDeParada.length;idx++){
+            int ponto = pontosDeParada[idx];
+            if(posicaoAtual + offset  >= ponto && posicaoAtual + offset  < L){
+                if(posicaoAtual == 18)
+                    System.out.println("ponto!");
+                resp = ponto;
             }
         }
-        return result;
+        
+        return resp;
+        
     }
 }
